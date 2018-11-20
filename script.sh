53826eb0bff2b86df0b0311687660959e9442dac
Denov <denov@Denovs-MacBook-Pro.local>
2018-11-20 12:55:03 +0200

    edit files

--- script.sh
+++ script.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-# Install kubectl 
-sudo apt-get update && sudo apt-get install -y apt-transport-https 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list 
-sudo apt-get update 
-sudo apt-get install -y kubectl
-
-# Install go 
-GOVER=1.11.2
+apt-get install unzip
 
 # https://golang.org/dl/ 
-curl https://dl.google.com/go/go${GOVER}.linux-amd64.tar.gz -O 
-sudo tar -xvf go${GOVER}.linux-amd64.tar.gz 
-sudo mv go /usr/local/bin/go
+curl https://releases.hashicorp.com/consul/1.4.0/consul_1.4.0_linux_amd64.zip -O 
+sudo unzip consul_1.4.0_linux_amd64.zip 
+sudo mv consul /usr/local/bin/consul
 hash -r
 
+consul -v
 
-# Install aws-iam-authenticator 
-go get -u -v github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator
