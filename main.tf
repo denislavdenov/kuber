0ffb7a72d26274e10a2058dfcb123ef246c85280
Denov <denov@Denovs-MacBook-Pro.local>
2018-11-20 10:24:24 +0200

    add files

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "null_resource" "local_install-3" { 
+provisioner "local-exec" { 
+command = "./script.sh" 
+}
+provisioner "local-exec" {
+command = "echo $PATH"             
+}
+}
+resource "null_resource" "local-4" {
+provisioner "local-exec" {
+command = "echo $PATH"
+}
+} 
---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install kubectl 
+sudo apt-get update && sudo apt-get install -y apt-transport-https 
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list 
+sudo apt-get update 
+sudo apt-get install -y kubectl
+
+# Install go 
+GOVER=1.11.2
+
+# https://golang.org/dl/ 
+curl https://dl.google.com/go/go${GOVER}.linux-amd64.tar.gz -O 
+sudo tar -xvf go${GOVER}.linux-amd64.tar.gz 
+sudo mv go /usr/local
+
+mkdir ~/go
+
+echo "export GOROOT=/usr/local/go" ~/.profile 
+echo 'export GOPATH=$HOME/go' >> ~/.profile 
+echo 'export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin' >> ~/.profile
+
+source ~/.profile
+
+# Install aws-iam-authenticator 
+go get -u -v github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator
